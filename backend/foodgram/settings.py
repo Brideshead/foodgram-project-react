e041a60aa084192631c5314490422799b241e38d
Brideshead <alexander.bogoevich@gmail.com>
2023-06-24 20:08:28 +0200

    test25

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -1,17 +1,22 @@
 import os
 
-BASE_DIR = os.path.dirname(
-    os.path.dirname(os.path.abspath(__file__)),
-)
-
 SECRET_KEY = os.getenv(
     'SECRET_KEY',
     default='123',
 )
 
+BASE_DIR = os.path.dirname(
+    os.path.dirname(os.path.abspath(__file__)),
+)
+
+
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '51.250.87.5',
+]
 
 AUTH_USER_MODEL = 'users.User'
 
@@ -61,24 +66,35 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'foodgram.wsgi.application'
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     },
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE',
-                            default='django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT')
-    }
+        'ENGINE': os.getenv(
+            'DB_ENGINE',
+            default='django.db.backends.postgresql',
+        ),
+        'NAME': os.getenv(
+            'DB_NAME',
+            default='postgres',
+        ),
+        'USER': os.getenv(
+            'POSTGRES_USER',
+            default='postgres',
+        ),
+        'PASSWORD': os.getenv(
+            'POSTGRES_PASSWORD',
+            default='postgres',
+        ),
+        'HOST': os.getenv(
+            'DB_HOST',
+            default='db',
+        ),
+        'PORT': os.getenv(
+            'DB_PORT',
+            default='5432',
+        ),
+    },
 }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
